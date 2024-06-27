cb47339072f57e7bb572e918d8904a7f936919aa
João Zunino <mr.joaozunino@gmail.com>
2024-06-26 22:09:41 -0300

    i make some js codes and a cars quiz

---
+++ JavaScript/main.js
@@ -0,0 +1,3 @@
+window.alert('My first mensage')
+window.confirm('Are you like Cars 2?')
+window.prompt('Whats the best Cars film?')
---
+++ Pratic/main.js
@@ -0,0 +1,3 @@
+window.alert('Você vai entrar nesse site')
+window.confirm('Você tem certeza que quer entrar?')
+window.prompt('Qual o número do seu cartão de crédito?')
