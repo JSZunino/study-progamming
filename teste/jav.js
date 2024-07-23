34a68e9fafe84d7b9620cb684b4b159e8ffe1410
João Zunino <mr.joaozunino@gmail.com>
2024-07-23 01:15:19 -0300

    i make some exercises in JS

--- teste/jav.js
+++ teste/jav.js
@@ -5,6 +5,14 @@ function calcular() {
     res.innerHTML = `<p>Sua velocidade atual é de ${vel}Km/h</p>`
     if (vel > 60) {
         res.innerHTML += `<p>Você está multado por excesso de velocidade</p>`
+    } else {
+        if (vel == 13) {
+            res.innerHTML += `<p>tu é petista? fez o L é?</p>`
+        }
     }
     res.innerHTML += `<p>Dirija sempre com sinto de segurança!</p>`
 }
+
+var agora = new Date()
+var hora = agora.getHours()
+document.write (`Agora são exatamente ${hora} horas`)
