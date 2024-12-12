0bd2364386c655a70e293d4526f6ad09493daa2a
João Zunino <mr.joaozunino@gmail.com>
2024-12-12 13:42:41 -0300

    i learn some fundaments

--- Java/MeuPrimeiroProjeto/src/MeuPrimeiroPrograma.java
+++ Java/MeuPrimeiroProjeto/src/MeuPrimeiroPrograma.java
@@ -1,7 +1,12 @@
 public class MeuPrimeiroPrograma {
 
     public static void main(String[] args) {
-        System.out.println("Ola mundo");
+        System.out.println("Eu amo café");
+        System.out.println(2 + 2);
+
+        // comentarios sao uteis
+        // calcular
+
     }
 
 }
